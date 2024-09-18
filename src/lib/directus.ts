4d20bc89ea4ce9187e2d6b0fbc76fb687b60ad1a
alex8a1991 <aochoa@copeco.gob.hn>
2024-09-18 14:15:07 -0600

    cambios 18 de septiembre

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -70,11 +70,72 @@ type EXPEDIENTES = {
     Nombre_del_expediente: any
 }
 
+type cocoin = {
+    id:                         number;
+    pregunta_que_es_cocoin:     string;
+    definicion_de_cocoin:       string;
+    pregunta_quienes_integran:  string;
+    quienes_integran_el_cocoin: string;
+    imagen_1_banner:            string;
+    imagen_2_banner:            string;
+    plan_de_trabajo:            string;
+    Funciones_del_COCOIN:       string;
+    definicion_Funciones_del_COCOIN:    string;
+}
+
+
+type DCGNE = {
+    id:                         number;
+    logo:                       string;
+    Mision:                     string;
+    Vision:                     string;
+    carouseldcge1:               string;
+    carouseldcge2:               string;
+    carouseldcge3:               string;
+    imagen_mision:               string;
+    imagen_vision:               string;
+    imagen_usar:                 string;
+    ejes_usar:                   string;
+    usar_definicion:             string;
+    definicion_parnuh:             string;
+    parches_usar:             string;
+    definicion_scuh:             string;
+    imagen_scuh:             string;
+    ciclo_operaciones_usar:             string;
+    grupo_usar_acreditados:             string;
+}
+
+type comite_probidad_y_etica = {
+    id:                    number;
+    logo:                   string;
+    Quienes_somos:         string;
+    imagen_banner1:        string;
+    mision:                string;
+    vision:                string;
+    nuestro_objetivo:      string;
+    acta_de_juremantacion: string;
+    plan_de_trabajo:       string;
+    presidente:            string;
+    foto_presidente:       string;
+    secretario:            string;
+    foto_secretario:       string;
+    primera_vocal:         string;
+    segunda_vocal:         string;
+    foto_segunda_vocal:    string;
+    tercera_vocal:         string;
+    foto_tercera_vocal:    string;
+    acta_de_jurementacion: string;
+    foto_primera_vocal:    string;
+    imagen_comite:          string;
+}
 
 type Schema = {
   articles: Articles[];
   alertas: Alerta;
   header: Header;
+  cocoin:cocoin;
+  DCGNE:DCGNE;
+  comite_probidad_y_etica:comite_probidad_y_etica;
   pronostico: Pronostico;
   EXPEDIENTES: EXPEDIENTES[];
 }

dd0ad3f2725aae160041b9bb8216de6c0cbf1569
alex8a1991 <aochoa@copeco.gob.hn>
2024-10-25 11:15:38 -0600

    CAMBIOS 25 OCT 2024

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 
+
 import vercel from "@astrojs/vercel";
-import vercelServerless from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercelServerless(),
+  adapter: vercel(),
+    
   integrations: [react()]
 });
